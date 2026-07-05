5e6af0dd95d64f26ce84040a82cba08be3221db4
jtmlsty9917 <jtmlsty9917@gmail.com>
2026-07-05 13:11:54 +0800

    deploy with correct base path [skip ci]

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/jkzs_ll/sw.js', { scope: '/jkzs_ll/' })})}
